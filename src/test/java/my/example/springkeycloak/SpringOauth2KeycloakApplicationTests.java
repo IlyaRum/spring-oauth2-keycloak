6aa1ddd38bc5b99dc84ae19bba9be59e47f458b4
IlyaRum <tekstir@yandex.ru>
2025-01-28 17:13:39 +0300

    init

---
+++ src/test/java/my/example/springkeycloak/SpringOauth2KeycloakApplicationTests.java
@@ -0,0 +1,13 @@
+package my.example.springkeycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOauth2KeycloakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
